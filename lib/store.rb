d1fa6e2d8fdf2291c618921809783effc1831187
Emily Dong <emilyhfdong@gmail.com>
2018-07-17 19:55:00 +0000

    disallow store destruction

--- lib/store.rb
+++ lib/store.rb
@@ -5,10 +5,21 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
   validate :either_womens_or_mens_apparel
 
-  def either_womens_or_mens_apparel
-    if womens_apparel == false && mens_apparel == false
-      errors.add(:womens_apparel, "must be either women's or men's apparel")
+  before_destroy :check_destroy
+
+  private
+    def either_womens_or_mens_apparel
+      if womens_apparel == false && mens_apparel == false
+        errors.add(:womens_apparel, "must be either women's or men's apparel")
+      end
     end
-  end
+
+    def check_destroy
+      if self.employees.size > 0
+        throw :abort
+      end
+    end
+
+
 
 end
